691b2c13f19263fd9ea5860b625d3dc2e566db35
Akash kumar <162474542+Akashkumar8228@users.noreply.github.com>
2024-08-08 12:00:41 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,13 @@
+class P1
+{
+    public static void main(String[] args) {
+    
+   System.out.println();
+   System.out.println();
+   System.out.println();
+   System.out.println();
+   System.out.println();
+   System.out.println();
+   System.out.println();
+    }
+    }
